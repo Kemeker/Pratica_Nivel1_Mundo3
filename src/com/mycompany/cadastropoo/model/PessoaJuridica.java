89a5bf3f403a8d0c98840c6b6bb86d63c8ce70de
Kemeker <kemeker@gmail.com>
2023-09-02 20:51:44 -0300

    teste

---
+++ src/com/mycompany/cadastropoo/model/PessoaJuridica.java
@@ -0,0 +1,32 @@
+package com.mycompany.cadastropoo.model;
+
+import java.io.Serializable;
+
+public class PessoaJuridica extends Pessoa implements Serializable {
+    private String cnpj;
+
+    public PessoaJuridica() {
+    }
+
+    public PessoaJuridica(int id, String nome, String cnpj) {
+        super(id, nome);
+        this.cnpj = cnpj;
+    }
+
+    
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public void exibir() {
+        super.exibir();
+        System.out.println("CNPJ: " + cnpj);
+    }
+}
+
+
---
+++ src/com/mycompany/cadastropoo/model/PessoaJuridicaRepo.java
@@ -0,0 +1,20 @@
+package com.mycompany.cadastropoo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PessoaJuridicaRepo {
+    private List<PessoaJuridica> pessoas;
+
+    public PessoaJuridicaRepo() {
+        pessoas = new ArrayList<>();
+    }
+
+    public void inserir(PessoaJuridica pessoa) {
+        pessoas.add(pessoa);
+    }
+
+   
+}
+
+
